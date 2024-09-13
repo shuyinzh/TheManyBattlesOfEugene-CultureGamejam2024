92092ebfbac1b747930f37d4e5488cf8e50a4fba
Nikolaus Kasyan <nikolaus.kasyan@voxtronic.com>
2024-09-13 14:07:42 +0200

    Add basic Deck functionality

--- Assets/Scripts/DeckSystem.cs
+++ Assets/Scripts/DeckSystem.cs
@@ -1,9 +1,12 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class DeckSystem : MonoBehaviour
 {
+    
+    private static int MAX_HAND_SIZE = 5;
+    private static int MAX_DECK_SIZE = 10;
+    
     private List<Card> deck;
     private List<Card> hand;
     private List<Card> discardPile;
@@ -19,4 +22,40 @@ public class DeckSystem : MonoBehaviour
     {
         
     }
+    
+    public void DrawCard()
+    {
+        if (deck.Count > 0)
+        {
+            hand.Add(deck[0]);
+            deck.RemoveAt(0);
+        }
+        else
+        {
+            ShuffleDiscardPileIntoDeck();
+            DrawCard();
+        }
+    }
+    
+    public void ShuffleDiscardPileIntoDeck()
+    {
+        deck.AddRange(discardPile);
+        discardPile.Clear();
+        ShuffleDeck();
+    }
+    
+    public void ShuffleDeck()
+    {
+        for (int i = 0; i < deck.Count; i++)
+        {
+            Card temp = deck[i];
+            int randomIndex = Random.Range(i, deck.Count);
+            deck[i] = deck[randomIndex];
+            deck[randomIndex] = temp;
+        }
+    }
+    
+    
+    
+    
 }
